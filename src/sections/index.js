303ae5c56a4d429d904fb168a493ed254148458f
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2024-06-13 18:36:41 -0400

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero"; 
+import PopularProducts from "./PopularProduct";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero, 
+    Services,
+    SuperQuality,
+    PopularProducts,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
