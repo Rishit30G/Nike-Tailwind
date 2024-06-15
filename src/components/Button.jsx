ff7cf9904bf1093369809fd4fe97169345b7d26a
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2024-06-15 11:18:08 -0400

    add sections

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({label, icon, backgroundColor, textColor, borderColor}) => {
+  return (
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none'
+
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor} rounded-full` : 
+    'bg-coral-red rounded-full text-white border-coral-red'}`}>
+        {label}
+       {icon && <img src={icon} alt="arrow" className="ml-2 rounded-full w-5 h-5"/>}
+    </button>
+  )
+}
+
+export default Button
